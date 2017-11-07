95b7714ef24033b032c158f3df1b1b9616268240
Nate <nschrein@nmu.edu>
2017-11-07 16:26:27 -0500

    NS

--- src/dataAbstraction/Journal.java
+++ src/dataAbstraction/Journal.java
@@ -4,5 +4,22 @@ import java.util.ArrayList;
 
 public class Journal {
 	public ArrayList<Trade> trades;
+	public JournalStats stats;
 
+	private Journal(ArrayList<Trade> trades) {
+		this.trades = trades;
+	}
+
+	public ArrayList<Trade> getTrades() {
+		return trades;
+	}
+
+	public void setTrades(ArrayList<Trade> trades) {
+		this.trades = trades;
+	}
+
+	public Trade getItem(int index) {
+		return trades.get(index);
+	}
 }
+
