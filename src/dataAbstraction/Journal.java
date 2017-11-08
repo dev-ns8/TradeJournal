2837db80290009d76274b2a4c32dc8d19f5ffb8e
Nate <nschrein@nmu.edu>
2017-11-08 14:05:34 -0500

    New addTrade() Architecture

--- src/dataAbstraction/Journal.java
+++ src/dataAbstraction/Journal.java
@@ -6,8 +6,11 @@ public class Journal {
 	public ArrayList<Trade> trades;
 	public JournalStats stats;
 
-	private Journal(ArrayList<Trade> trades) {
+	public Journal(ArrayList<Trade> trades) {
 		this.trades = trades;
+	}
+	public Journal() {
+		//Empty on purpose
 	}
 
 	public ArrayList<Trade> getTrades() {
@@ -20,6 +23,17 @@ public class Journal {
 
 	public Trade getItem(int index) {
 		return trades.get(index);
+	}
+
+	public void addTrade(Object args) {
+		// 1. Iterate through argument list and check the length of array
+		//2. If more than 5, create a Trade with the 5 mandatory arguments and pass the rest into helper method
+		/* 3. have helperMethod iterate through arguments and decipher what arguments it has and use setters to add to
+				the newley created Trade object	*/
+
+
+
+		Trade newTrade = new Trade(args);
 	}
 }
 
