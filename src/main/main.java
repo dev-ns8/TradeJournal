4bd56ddbb4b785a7408a5ef627d1a86ac6ca939f
Nate <nschrein@nmu.edu>
2017-11-14 19:37:53 -0500

    bb

---
+++ src/main/main.java
@@ -0,0 +1,18 @@
+package main;
+import dataAbstraction.Journal;
+import dataAbstraction.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        User nate = new User();
+        Journal testJournal = nate.getUser_journal();
+      //  testJournal.addTrade("/CL", "11-12-13", "52.45");
+
+
+    }
+}
