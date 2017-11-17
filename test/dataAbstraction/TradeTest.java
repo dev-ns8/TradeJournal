f0928f2c1486d55ce19adc6b4c38eae8a26e55d8
Nate <nschrein@nmu.edu>
2017-11-16 19:50:16 -0500

    J-Unit finally working on my MAC and all Test's GreenLight

--- test/dataAbstraction/TradeTest.java
+++ test/dataAbstraction/TradeTest.java
@@ -6,17 +6,17 @@ import org.junit.jupiter.api.Test;
 import utils.TradeHelper;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TradeTest {
-    private Trade basic,advanced;
+    private Trade basic,advanced, two;
 
     @BeforeEach
     void setUp() {
+        Date date = new GregorianCalendar(2017, Calendar.NOVEMBER, 11).getTime();
+        Date dateTwo = new GregorianCalendar(2017, Calendar.NOVEMBER, 12).getTime();
         basic = new Trade.TradeBuilder("CAPR",new Date(),new BigDecimal(1.0),true,1,false)
                 .build();
         advanced = new Trade.TradeBuilder("YUMA",new Date(),new BigDecimal(2.0),false,1,true)
@@ -28,6 +28,11 @@ class TradeTest {
                 .stopLoss(new BigDecimal(2.8))
                 .takeProfit(new BigDecimal(1.5))
                 .build();
+        two = new Trade.TradeBuilder("/ZB", date, new BigDecimal(152.06), true, 1, false)
+                .exit(dateTwo)
+                .exitPrice(new BigDecimal(153))
+                .build();
+        System.out.println(advanced.getTrade_duration());
     }
 
     @AfterEach
@@ -38,6 +43,8 @@ class TradeTest {
 
     @Test
     void updateTradeStats() {
+       // int days = Date.
+       // assertTrue(two.getTrade_duration() == )
         assertTrue(advanced.getPnl().compareTo(new BigDecimal(-1.0))==0);
         assertTrue(advanced.getTrade_duration().getSeconds()==0);
         assertTrue(!advanced.getWinner());
